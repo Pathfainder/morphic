dbe5fd4c66b3e13bcca2a65d0117ba18c2d51eef
Pathfainder <160763159+Pathfainder@users.noreply.github.com>
2024-05-26 23:55:38 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { Chat } from '@/components/chat'
+import { nanoid } from 'ai'
+import { AI } from './actions'
+import styles from './Page.module.css'
+
+export const maxDuration = 60
+
+export default function Page() {
+  const id = nanoid()
+  return (
+    <AI initialAIState={{ chatId: id, messages: [] }}>
+      <div className={styles.pageContainer}>
+        <div className={styles.logoPlaceholder}>Logo Placeholder</div>
+        <Chat id={id} />
+      </div>
+    </AI>
+  )
+}
